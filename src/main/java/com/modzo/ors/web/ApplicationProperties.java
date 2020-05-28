11b535c27f34fd3c5cce967985b37ab770ede9cb
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-05-29 01:14:36 +0300

    Configuration properties fixes

--- src/main/java/com/modzo/ors/web/ApplicationProperties.java
+++ src/main/java/com/modzo/ors/web/ApplicationProperties.java
@@ -2,72 +2,69 @@ package com.modzo.ors.web;
 
 import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.Duration;
 
-@Configuration
 @ConfigurationProperties(prefix = "application")
+@Validated
+@ConstructorBinding
 public class ApplicationProperties {
-
-    @NotBlank
-    private String apiUrl;
-
-    @NotNull
-    private Sitemap sitemap;
 
     public static class Sitemap {
 
         @NotNull
-        private Integer itemsPerPage;
+        private final Integer itemsPerPage;
 
         @NotNull
-        private Duration cacheTime;
+        private final Duration cacheTime;
 
         @URL
         @NotNull
-        private String domain;
+        private final String domain;
+
+        public Sitemap(Integer itemsPerPage,
+                       Duration cacheTime,
+                       String domain) {
+            this.itemsPerPage = itemsPerPage;
+            this.cacheTime = cacheTime;
+            this.domain = domain;
+        }
 
         public Integer getItemsPerPage() {
             return itemsPerPage;
-        }
-
-        public void setItemsPerPage(Integer itemsPerPage) {
-            this.itemsPerPage = itemsPerPage;
         }
 
         public Duration getCacheTime() {
             return cacheTime;
         }
 
-        public void setCacheTime(Duration cacheTime) {
-            this.cacheTime = cacheTime;
-        }
-
         public String getDomain() {
             return domain;
         }
+    }
 
-        public void setDomain(String domain) {
-            this.domain = domain;
-        }
+    @NotBlank
+    private final String apiUrl;
+
+    @NotNull
+    private final Sitemap sitemap;
+
+    public ApplicationProperties(String apiUrl, Sitemap sitemap) {
+        this.apiUrl = apiUrl;
+        this.sitemap = sitemap;
     }
 
     public String getApiUrl() {
         return apiUrl;
     }
 
-    public void setApiUrl(String apiUrl) {
-        this.apiUrl = apiUrl;
-    }
-
     public Sitemap getSitemap() {
         return sitemap;
-    }
-
-    public void setSitemap(Sitemap sitemap) {
-        this.sitemap = sitemap;
     }
 }
