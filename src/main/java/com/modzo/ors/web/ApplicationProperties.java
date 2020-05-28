ca24defd01656b826606601a229206c29fc4c055
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-05-28 22:59:11 +0300

    Caching

--- src/main/java/com/modzo/ors/web/ApplicationProperties.java
+++ src/main/java/com/modzo/ors/web/ApplicationProperties.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.Duration;
 
 @Configuration
 @ConfigurationProperties(prefix = "application")
@@ -12,6 +14,34 @@ public class ApplicationProperties {
     @NotBlank
     private String apiUrl;
 
+    @NotNull
+    private Sitemap sitemap;
+
+    public static class Sitemap {
+
+        @NotNull
+        private Integer itemsPerPage;
+
+        @NotNull
+        private Duration cacheTime;
+
+        public Integer getItemsPerPage() {
+            return itemsPerPage;
+        }
+
+        public void setItemsPerPage(Integer itemsPerPage) {
+            this.itemsPerPage = itemsPerPage;
+        }
+
+        public Duration getCacheTime() {
+            return cacheTime;
+        }
+
+        public void setCacheTime(Duration cacheTime) {
+            this.cacheTime = cacheTime;
+        }
+    }
+
 
     public String getApiUrl() {
         return apiUrl;
@@ -19,5 +49,13 @@ public class ApplicationProperties {
 
     public void setApiUrl(String apiUrl) {
         this.apiUrl = apiUrl;
+    }
+
+    public Sitemap getSitemap() {
+        return sitemap;
+    }
+
+    public void setSitemap(Sitemap sitemap) {
+        this.sitemap = sitemap;
     }
 }
--- src/main/java/com/modzo/ors/web/sitemap/root/SitemapRootService.java
+++ src/main/java/com/modzo/ors/web/sitemap/root/SitemapRootService.java
@@ -1,23 +1,44 @@
 package com.modzo.ors.web.sitemap.root;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.modzo.ors.web.ApplicationProperties;
 import com.modzo.ors.web.api.radio.stations.RadioStationResponse;
 import com.modzo.ors.web.api.radio.stations.RadioStationsClient;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 class SitemapRootService {
 
+    private final static String ROOT_SITEMAP_KEY = "ROOT";
+
+    private final Cache<String, String> cache;
+
     private final RadioStationsClient radioStationsClient;
 
-    SitemapRootService(RadioStationsClient radioStationsClient) {
+    SitemapRootService(RadioStationsClient radioStationsClient, ApplicationProperties properties) {
         this.radioStationsClient = radioStationsClient;
+        this.cache = Caffeine.newBuilder()
+                .expireAfterWrite(properties.getSitemap().getCacheTime())
+                .maximumSize(1)
+                .build();
     }
 
-    String rootSitemap(){
+    String rootSitemap() {
+        return Optional.ofNullable(cache.getIfPresent(ROOT_SITEMAP_KEY))
+                .orElseGet(this::buildAndCacheSitemap);
+    }
+
+    private String buildAndCacheSitemap() {
         PagedModel<EntityModel<RadioStationResponse>> radioStations = radioStationsClient.getRadioStations(0);
-        return String.valueOf(radioStations.getMetadata().getTotalElements());
+
+        String sitemap = String.valueOf(radioStations.getMetadata().getTotalElements());
+        cache.put(ROOT_SITEMAP_KEY, sitemap);
+        return sitemap;
     }
 
 }
