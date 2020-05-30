649397760afa7ce4f289d4504d9f0d125e474db7
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-05-31 00:26:02 +0300

    Radio station page test

---
+++ src/test/groovy/com/modzo/ors/web/radio/stations/RadioStationControllerSpec.groovy
@@ -0,0 +1,29 @@
+package com.modzo.ors.web.radio.stations
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.modzo.ors.web.IntegrationSpec
+import org.junit.Rule
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+class RadioStationControllerSpec extends IntegrationSpec {
+
+    @Rule
+    WireMockRule wireMockRule = wireMockRule('stubs/radio-stations')
+
+    void 'should retrieve radio stations page'() {
+        when:
+            ResponseEntity<String> result = restTemplate.getForEntity('/radio-stations/the-hits-radio/5', String)
+        then:
+            result.statusCode == HttpStatus.OK
+        and:
+            result.body.contains('src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>')
+            result.body.contains('<span>Some google ads</span>')
+        and:
+            result.body.contains('<h1 class="title">&quot;The Hits&quot; Radio')
+            result.body.contains('<source src="http://listen.radionomy.com:80/-TheHits-Radio" type="audio/ogg">')
+        and:
+            result.body.contains('<h2>Latest searches</h2>')
+            result.body.contains('<a href="/search/by-radio-station/hits">hits</a>')
+    }
+}
