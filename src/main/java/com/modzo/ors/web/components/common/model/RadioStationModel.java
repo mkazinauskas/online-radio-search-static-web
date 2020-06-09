3771793c25fc1320367dbd85b99da778b1014b9b
modestas.kazinauskas <modestukasai@gmail.com>
2020-06-09 23:00:25 +0300

    Genres removal (#8)
    Co-authored-by: modestas.kazinauskas <modestas.kazinauskas@gmail.com>

--- src/main/java/com/modzo/ors/web/components/common/model/RadioStationModel.java
+++ src/main/java/com/modzo/ors/web/components/common/model/RadioStationModel.java
@@ -3,10 +3,6 @@ package com.modzo.ors.web.components.common.model;
 import com.modzo.ors.web.api.radio.stations.RadioStationResponse;
 import com.modzo.ors.web.api.search.radio.station.SearchRadioStationResultResponse;
 import com.modzo.ors.web.utils.SeoText;
-import org.springframework.util.CollectionUtils;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class RadioStationModel {
 
@@ -20,26 +16,12 @@ public class RadioStationModel {
 
     private final String website;
 
-    private final List<GenreModel> genres;
-
-    public RadioStationModel(long id, String uniqueId, String title, String website, List<GenreModel> genres) {
-        this.id = id;
-        this.uniqueId = uniqueId;
-        this.title = title;
-        this.seoTitle = SeoText.from(title);
-        this.website = website;
-        this.genres = genres;
-    }
-
     public RadioStationModel(RadioStationResponse response) {
         this.id = response.getId();
         this.uniqueId = response.getUniqueId();
         this.title = response.getTitle();
         this.seoTitle = SeoText.from(response.getTitle());
         this.website = response.getWebsite();
-        this.genres = response.getGenres().stream()
-                .map(GenreModel::new)
-                .collect(Collectors.toList());
     }
 
     public RadioStationModel(SearchRadioStationResultResponse response) {
@@ -48,9 +30,6 @@ public class RadioStationModel {
         this.title = response.getTitle();
         this.seoTitle = SeoText.from(response.getTitle());
         this.website = response.getWebsite();
-        this.genres = response.getGenres().stream()
-                .map(GenreModel::new)
-                .collect(Collectors.toList());
     }
 
     public long getId() {
@@ -73,11 +52,4 @@ public class RadioStationModel {
         return website;
     }
 
-    public List<GenreModel> getGenres() {
-        return genres;
-    }
-
-    public boolean hasGenres() {
-        return !CollectionUtils.isEmpty(genres);
-    }
 }
