40247348b634b82ed5083b8366ed35b3d8dc5f52
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-06-09 21:14:22 +0300

    Genres removal

--- src/main/java/com/modzo/ors/web/api/search/radio/station/SearchRadioStationResultResponse.java
+++ src/main/java/com/modzo/ors/web/api/search/radio/station/SearchRadioStationResultResponse.java
@@ -3,8 +3,6 @@ package com.modzo.ors.web.api.search.radio.station;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
-
 public class SearchRadioStationResultResponse {
 
     private final long id;
@@ -15,19 +13,15 @@ public class SearchRadioStationResultResponse {
 
     private final String website;
 
-    private final List<GenreResponse> genres;
-
     @JsonCreator
     private SearchRadioStationResultResponse(@JsonProperty("id") long id,
                                              @JsonProperty("uniqueId") String uniqueId,
                                              @JsonProperty("title") String title,
-                                             @JsonProperty("website") String website,
-                                             @JsonProperty("genres") List<GenreResponse> genres) {
+                                             @JsonProperty("website") String website) {
         this.id = id;
         this.uniqueId = uniqueId;
         this.title = title;
         this.website = website;
-        this.genres = genres;
     }
 
     public long getId() {
@@ -46,34 +40,4 @@ public class SearchRadioStationResultResponse {
         return website;
     }
 
-    public List<GenreResponse> getGenres() {
-        return genres;
-    }
-
-    public static class GenreResponse {
-
-        private final long id;
-
-        private final String uniqueId;
-
-        private final String title;
-
-        public GenreResponse(long id, String uniqueId, String title) {
-            this.id = id;
-            this.uniqueId = uniqueId;
-            this.title = title;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public String getUniqueId() {
-            return uniqueId;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-    }
 }
--- src/main/java/com/modzo/ors/web/components/common/model/GenreModel.java
+++ src/main/java/com/modzo/ors/web/components/common/model/GenreModel.java
@@ -1,9 +1,7 @@
 package com.modzo.ors.web.components.common.model;
 
 import com.modzo.ors.web.api.genres.GenreResponse;
-import com.modzo.ors.web.api.radio.stations.RadioStationResponse;
 import com.modzo.ors.web.api.search.genre.SearchGenreResultResponse;
-import com.modzo.ors.web.api.search.radio.station.SearchRadioStationResultResponse;
 import com.modzo.ors.web.utils.SeoText;
 
 public class GenreModel {
@@ -13,18 +11,6 @@ public class GenreModel {
     private final String title;
 
     private final String seoTitle;
-
-    public GenreModel(SearchRadioStationResultResponse.GenreResponse genreResponse) {
-        this.id = genreResponse.getId();
-        this.title = genreResponse.getTitle();
-        this.seoTitle = SeoText.from(title);
-    }
-
-    public GenreModel(RadioStationResponse.GenreResponse genreResponse) {
-        this.id = genreResponse.getId();
-        this.title = genreResponse.getTitle();
-        this.seoTitle = SeoText.from(title);
-    }
 
     public GenreModel(SearchGenreResultResponse genreResponse) {
         this.id = genreResponse.getId();
